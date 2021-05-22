7a836a8f0ba0ebb472f9c5d3d55780281fb3c9cf
Trần Thăng <ngocthangtrantb@gmail.com>
2021-05-23 02:41:36 +0700

    complete slidebar menu

---
+++ design-html/javascript/app.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.sidebar-submenu').forEach(e=>{
+    e.querySelector('.sidebar-menu-dropdown').onclick = event=>{
+        event.preventDefault();
+        e.querySelector(".sidebar-menu-dropdown .dropdown-icon").classList.toggle('active')
+
+
+        let dropdown_content = e.querySelector('.sidebar-menu-dropdown-conten')
+        let dropdown_content_lis = dropdown_content.querySelectorAll('li')
+
+        let active_height = dropdown_content_lis[0].clientHeight * dropdown_content_lis.length
+        console.log(dropdown_content_lis[0].clientHeight)
+
+        dropdown_content.classList.toggle('active')
+
+        dropdown_content.style.height = dropdown_content.classList.contains('active') ? active_height + 'px' : '0'
+    }
+})
