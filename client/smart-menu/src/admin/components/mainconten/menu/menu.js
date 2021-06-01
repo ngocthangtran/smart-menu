2122318bbebfa18025d4066659c02a0163e212be
Trần Thăng <ngocthangtrantb@gmail.com>
2021-06-01 16:38:38 +0700

    add card-food to client/smart-menu

--- client/smart-menu/src/admin/components/mainconten/menu/menu.js
+++ client/smart-menu/src/admin/components/mainconten/menu/menu.js
@@ -6,93 +6,160 @@ class menu extends Component {
     render() {
         return (
             <>
-                <div className="category">
-                    <div className="category-title">
-                        <div className="name">
-                            Hấp
-                </div>
-                        <div className="icon-category">
-                            <i className='bx bxs-chevron-down'></i>
+                <div class="category-food">
+                    <div class="category-head">
+                        Hấp
+        </div>
+                    <div class="category-item">
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="cards-menu">
-                        <div className="card-menu">
-                            <img src="https://cdn.caythuocdangian.com/2019/05/de-hap-la-tia-to.jpg" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptas at veritatis similique
-                                harum quos culpa. Tempora enim maxime unde distinctio, tenetur cum vero amet consectetur debitis,
-                                totam inventore repellat.
-                </div>
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
-                        <div className="card-menu">
-                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iure nostrum inventore enim tempora
-                                laboriosam maiores minima maxime quos ipsum nam quidem ab natus autem odit, quae provident atque
-                                earum.
-                </div>
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
-                        <div className="card-menu">
-                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eveniet tempora quasi itaque, ex
-                                esse, quos magni illum expedita, fuga ut. Aperiam totam expedita qui harum, voluptas ullam earum
-                                quis.
-                </div>
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
-                        <div className="card-menu">
-                            <img src="https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg"
-                                alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laboriosam quos facilis dolorum
-                                corrupti beatae aspernatur, temporibus nobis placeat odio consequuntur eos, nisi voluptas,
-                                distinctio dolorem inventore consequatur molestiae perspiciatis!
-                </div>
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="category">
-                    <div className="category-title">
-                        <div className="name">
-                            Hấp
-                </div>
-                        <div className="icon-category">
-                            <i className='bx bxs-chevron-down'></i>
-                        </div>
-                    </div>
-                    <div className="cards-menu">
-                        <div className="card-menu">
-                            <img src="https://cdn.caythuocdangian.com/2019/05/de-hap-la-tia-to.jpg" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptas at veritatis similique
-                                harum quos culpa. Tempora enim maxime unde distinctio, tenetur cum vero amet consectetur debitis,
-                                totam inventore repellat.
-                </div>
-                        </div>
-                        <div className="card-menu">
-                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iure nostrum inventore enim tempora
-                                laboriosam maiores minima maxime quos ipsum nam quidem ab natus autem odit, quae provident atque
-                                earum.
-                </div>
-                        </div>
-                        <div className="card-menu">
-                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eveniet tempora quasi itaque, ex
-                                esse, quos magni illum expedita, fuga ut. Aperiam totam expedita qui harum, voluptas ullam earum
-                                quis.
-                </div>
-                        </div>
-                        <div className="card-menu">
-                            <img src="https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg"
-                                alt="" className="card-image"></img>
-                            <div className="card-conten">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laboriosam quos facilis dolorum
-                                corrupti beatae aspernatur, temporibus nobis placeat odio consequuntur eos, nisi voluptas,
-                                distinctio dolorem inventore consequatur molestiae perspiciatis!
-                </div>
+                        <div class="food-card">
+                            <img src="https://fakeimg.pl/400x300/009578/fff/" alt=""></img>
+                            <div class="card-body">
+                                <h5>Dê hấp xả tía tô</h5>
+                                <div class="food-info">
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Phân loại:
+                            </div>
+                                        <div class="text-conten">
+                                            Hấp
+                            </div>
+                                    </div>
+                                    <div class="food-conten">
+                                        <div class="text-title">
+                                            Giá:
+                            </div>
+                                        <div class="text-conten">
+                                            Từ 150k
+                            </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn-detail">Chi tiết</a>
+                            </div>
                         </div>
                     </div>
                 </div>
